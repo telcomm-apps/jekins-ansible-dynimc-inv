13b510a1f99fdc22287c03d29beb40e4526846bc
sahityavumma <44340737+sahityavumma@users.noreply.github.com>
2023-01-23 17:25:44 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,14 @@
-provider "aws"{
-   region  = "ap-southeast-1" 
+provider "aws" {
+    region = "ap-south-1"
 }
-
-resource "aws_instance" "AWSEC2Instance"{
-     count   = 1
-     ami = "ami-010d564ad44bd1912"
-     instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
-     tags = {
-        Name = "tomcatservers"
-     }
+resource "aws_instance" "AWSinstance" {
+    ami = "ami-of9d9a251c1a44858"
+    instance_type = "t2.micro"
+    security_groups = ["Launch-wizard-2"]
+    key_name = "sahityapanakam"
+    tags = {
+    Name = "tomcatservers"
+    
+    }
 }
 
