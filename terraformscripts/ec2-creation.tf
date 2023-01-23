ca7689e489f5b6802f157637cc190728e9677132
sahityavumma <44340737+sahityavumma@users.noreply.github.com>
2023-01-23 18:16:46 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_instance" "AWSinstance" {
-    ami = "ami-of9d9a251c1a44858"
+    ami = "ami-0f9d9a251c1a44858"
     instance_type = "t2.micro"
     security_groups = ["Launch-wizard-2"]
     key_name = "sahityapanakam"
